98d2a17586337391c680064d00ec96e7ba746259
subtropicaldog <65210014+subtropicaldog@users.noreply.github.com>
2020-12-15 01:58:12 +0100

    加了些帮助理解的注释

--- svo/src/feature_detection.cpp
+++ svo/src/feature_detection.cpp
@@ -29,14 +29,14 @@ AbstractDetector::AbstractDetector(
     const int n_pyr_levels) :
         cell_size_(cell_size),
         n_pyr_levels_(n_pyr_levels),
-        grid_n_cols_(ceil(static_cast<double>(img_width)/cell_size_)),
+        grid_n_cols_(ceil(static_cast<double>(img_width)/cell_size_)),             // ceil 天花板 顾名思义向上取整函数
         grid_n_rows_(ceil(static_cast<double>(img_height)/cell_size_)),
         grid_occupancy_(grid_n_cols_*grid_n_rows_, false)
 {}
 
 void AbstractDetector::resetGrid()
 {
-  std::fill(grid_occupancy_.begin(), grid_occupancy_.end(), false);
+  std::fill(grid_occupancy_.begin(), grid_occupancy_.end(), false);             //fill 函数在范围内添加值 false
 }
 
 //* 将特征fts所在网格设置为occupancy
@@ -44,7 +44,7 @@ void AbstractDetector::setExistingFeatures(const Features& fts)
 {
   std::for_each(fts.begin(), fts.end(), [&](Feature* i){
     grid_occupancy_.at(
-        static_cast<int>(i->px[1]/cell_size_)*grid_n_cols_
+        static_cast<int>(i->px[1]/cell_size_)*grid_n_cols_                  // static——cast 就是一种类型转换 具体可以csdn
         + static_cast<int>(i->px[0]/cell_size_)) = true;
   });
 }
@@ -71,16 +71,16 @@ void FastDetector::detect(
     Features& fts)
 {
   //? 每个grid里面一个特征点
-  Corners corners(grid_n_cols_*grid_n_rows_, Corner(0,0,detection_threshold,0,0.0f));
+  Corners corners(grid_n_cols_*grid_n_rows_, Corner(0,0,detection_threshold,0,0.0f));       // cols* rows 是 Corners这个vector的size 后面的Corner是 Corner这个结构体的构造函数
   //* 对每一金字塔层进行循环 
   for(int L=0; L<n_pyr_levels_; ++L)
   {
-    const int scale = (1<<L);
+    const int scale = (1<<L);                 // << 是 二进制左移运算符 如果 L =1 则 scale =2 ，L=2 则 scale=4
     vector<fast::fast_xy> fast_corners;
     //* 检测fast角点 
 #if __SSE2__
       fast::fast_corner_detect_10_sse2(
-          (fast::fast_byte*) img_pyr[L].data, img_pyr[L].cols,
+          (fast::fast_byte*) img_pyr[L].data, img_pyr[L].cols,    // 在不同的金字塔层检测fast点
           img_pyr[L].rows, img_pyr[L].cols, 20, fast_corners);
 #elif HAVE_FAST_NEON
       fast::fast_corner_detect_9_neon(
